23d3092e46393e95996dfc98afa6a3c4d9c0f2ce
Liam <liam.jb1@gmail.com>
2018-05-20 20:06:46 +1200

    Start defining formatting process

--- lib/gen/generator_process.rb
+++ lib/gen/generator_process.rb
@@ -9,9 +9,21 @@ class GeneratorProcess
     end
 
     def format_schema(schema)
+        _schema = []
+        atrbs = schema[2...schema.length - 1]
+        atrbs.split(',').each do |a|
+            _schema.push(a)
+        end
+        _schema
     end
 
     def format_fds(fds)
+        _fds = {}
+        deps = fds[1...fds.length - 1]
+        deps.split('/').each do |d|
+            # TODO
+        end
+        _fds
     end
 
 end
