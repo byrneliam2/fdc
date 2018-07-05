3050be868ec53103a1fd3c4a2dc861516d0f78ef
Liam <liam.jb1@gmail.com>
2018-07-05 19:40:12 +1200

    Closure testing

--- lib/tests/tc_closure.rb
+++ lib/tests/tc_closure.rb
@@ -7,6 +7,72 @@
 require "test/unit"
 
 require_relative "../gen/closure"
+require_relative "../io/printer"
 
 class ClosureTests < Test::Unit::TestCase
+
+  def test01_ClosureRestrictionFormatting
+    c = Closure.new(nil, nil, nil)
+    assert_true(c.format_rests("[A]") == Set[["A"]])
+  end
+
+  def test02_ClosureRestrictionFormatting
+    c = Closure.new(nil, nil, nil)
+    assert_true(c.format_rests("[A,B;C,D]") == Set[["A", "B"], ["C", "D"]])
+  end
+
+  def test03_ClosureComputation
+    c = Closure.new("R(A,B,C}", "{A,B/C}", [])
+    x = {["A"] => Set["A"], ["B"] => Set["B"], ["C"] => Set["C"], 
+      ["A", "B"] => Set["A", "B", "C"], ["A", "C"] => Set["A", "C"], ["B", "C"] => Set["B", "C"], 
+      ["A", "B", "C"] => Set["A", "B", "C"]}
+    y = c.compute
+    assert_true(x.size == y.size) # ensure all cases covered
+    (x.keys & y.keys).each { |k| assert_true(x[k] == y[k]) }
+  end
+
+  def test04_ClosureComputation
+    c = Closure.new("R(A,B,C,D}", "{A/B,C;B/D}", [])
+    x = {["A"] => Set["A", "B", "C", "D"], ["B"] => Set["B", "D"], ["C"] => Set["C"], ["D"] => Set["D"],
+      ["A", "B"] => Set["A", "B", "C", "D"], ["A", "C"] => Set["A", "B", "C", "D"], 
+      ["A", "D"] => Set["A", "B", "C", "D"], ["B", "C"] => Set["B", "C", "D"], ["B", "D"] => Set["B", "D"], 
+      ["C", "D"] => Set["C", "D"],  ["A", "B", "C"] => Set["A", "B", "C", "D"], ["B", "C", "D"] => Set["B", "C", "D"],
+      ["A", "B", "D"] => Set["A", "B", "C", "D"], ["A", "C", "D"] => Set["A", "B", "C", "D"], 
+      ["A", "B", "C", "D"] => Set["A", "B", "C", "D"]}
+    y = c.compute
+    assert_true(x.size == y.size) # ensure all cases covered
+    (x.keys & y.keys).each { |k| assert_true(x[k] == y[k]) }
+  end
+
+  def test05_ClosuresWithRestrictions
+    c = Closure.new("R(A,B,C}", "{A,B/C}", "[A;B;C]")
+    x = {["A"] => Set["A"], ["B"] => Set["B"], ["C"] => Set["C"]}
+    y = c.compute
+    assert_true(x.size == y.size) # ensure all cases covered
+    (x.keys & y.keys).each { |k| assert_true(x[k] == y[k]) }
+  end
+
+  def test06_ClosuresWithRestrictions
+    c = Closure.new("R(ItemID,ItemName,Count}", "{ItemID/ItemName}", "[ItemName]")
+    x = {["ItemName"] => Set["ItemName"]}
+    y = c.compute
+    assert_true(x.size == y.size) # ensure all cases covered
+    (x.keys & y.keys).each { |k| assert_true(x[k] == y[k]) }
+  end
+
+  def test07_MinimalKeys
+    c = Closure.new("R(A,B,C,D,E}", "{A,B/D,E;B,C/D,E}", [])
+    assert_true(Set[["A", "B", "C"]] == c.compute_minimal_keys)
+  end
+
+  def test08_MinimalKeys
+    c = Closure.new("R(A,B,C,D}", "{A/B;C/D}", [])
+    assert_true(Set[["A", "C"]] == c.compute_minimal_keys)
+  end
+
+  def test09_MinimalKeys
+    c = Closure.new("R(A,B,C,D}", "{A/B,C,D;C/A,B,D}", [])
+    assert_true(Set[["A"], ["C"]] == c.compute_minimal_keys)
+  end
+
 end
--- lib/tests/tc_generator_process.rb
+++ lib/tests/tc_generator_process.rb
@@ -4,9 +4,10 @@
 require "test/unit"
 
 require_relative "../gen/generator_process"
-require_relative "../gen/closure"
 
 class GeneratorProcessTests < Test::Unit::TestCase
+
+  # Negative cases missing as parser should detect this before formatting.
 
   def test01_SchemaFormatting
     g = GeneratorProcess.new(nil, nil, nil)
@@ -28,28 +29,32 @@ class GeneratorProcessTests < Test::Unit::TestCase
     end
   end
 
-  def test11_FuncDependencies
+  def test04_FDFormatting
+    g = GeneratorProcess.new(nil, nil, nil)
+    x = [FuncDependency.new("EmployeeId", "EmployeeName,EmployeeDOB"), 
+      FuncDependency.new("EmployeeDOB", "CompanyCarQual")]
+    y = g.format_fds("{EmployeeId/EmployeeName,EmployeeDOB;EmployeeDOB/CompanyCarQual}").to_a 
+    for i in 0..x.length # use for loop to compare dependencies directly
+      assert_true(x[i] == y[i])
+    end
+  end
+
+  def test05_FuncDependencies
     f = FuncDependency.new("A", "B")
     assert_true(f.lhs == Set["A"])
     assert_true(f.rhs == Set["B"])
   end
 
-  def test12_FuncDependencies
+  def test06_FuncDependencies
     f = FuncDependency.new("A,B", "C,D")
     assert_true(f.lhs == Set["A", "B"])
     assert_true(f.rhs == Set["C", "D"])
   end
 
-  def test13_FuncDependencies
+  def test07_FuncDependencies
     f1 = FuncDependency.new("A,B", "C,D")
     f2 = FuncDependency.new("A,B", "C,D")
     assert_true(f1 == f2)
-  end
-
-  def test14_FuncDependencies
-    f1 = FuncDependency.new("A,B", "C,D")
-    f2 = FuncDependency.new("AB", "C,D")
-    assert_false(f1 == f2)
   end
 
 end
